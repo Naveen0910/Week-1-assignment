f26313fce322a7a3914ed4f223e9b083757ae58f
Naveen0910 <naveen850850@gmail.com>
2023-06-12 11:32:35 +0530

    TimesDone

--- 01-js/medium/times.js
+++ 01-js/medium/times.js
@@ -8,5 +8,15 @@ Hint - use Date class exposed in JS
 */
 
 function calculateTime(n) {
-    return 0.01;
+  const startTime = new Date();
+  let sum = 0;
+  for (var i = 1; i < n + 1; i++) {
+    sum = sum + i;
+  }
+  const endTime = new Date();
+  const executionTime = endTime - startTime;
+  console.log(sum);
+  return executionTime;
 }
+
+console.log(calculateTime(10000));
